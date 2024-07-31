6924804c0a9ef3457dabb982f5367cb14b8bbd34
sankalpaneupane <sneupane@hamropatro.com>
2024-07-31 14:38:10 +0545

    feature: edit profile

--- models/Post.js
+++ models/Post.js
@@ -13,6 +13,14 @@ const postSchema = new mongoose.Schema({
         type: String,
         default: "https://cdn.pixabay.com/photo/2024/06/28/10/04/beautiful-cities-in-west-africa-8859225_1280.jpg"
     },
+    country: {
+        type: String,
+        default: ""
+    },
+    city: {
+        type: String,
+        default: ""
+    },
     likes: [
         {
             type: mongoose.Schema.Types.ObjectId,
